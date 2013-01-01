3b633583224759cd86e359e93d2537c8d099a136
Dan Flettre <fletd01@yahoo.com>
2013-01-01 12:40:52 -0600

    reformat

--- src/com/mojang/mojam/buff/Buffs.java
+++ src/com/mojang/mojam/buff/Buffs.java
@@ -8,7 +8,8 @@ public class Buffs extends LinkedList<Buff> {
 
 	/* The order is FILO */
 	public boolean add(Buff buff) {
-		super.addFirst(buff); return true;
+		super.addFirst(buff); 
+		return true;
 	}
 	
 	/* Return combined value of all buff of "buffType" type */
